1698e633f4c24cc2d09d71beb66946d677236662
helusia <helusiasparkling@gmail.com>
2022-06-02 14:53:41 +0000

    新規投稿

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+  def index
+    @books=Book.all
+  end
+
+  def create
+    @book=Book.new
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  def show
+    @book=Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
+  def book_params
+      params.require(:book).permit(:title, :body)
+  end
+
 end
